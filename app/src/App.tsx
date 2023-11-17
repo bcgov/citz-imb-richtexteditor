776b9e98b88bf4785a3049bdaf6ba6968018f8f6
BradyMitch <braden.jr.mitch@gmail.com>
2023-11-17 11:13:39 -0800

    Added test application

---
+++ app/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import { RichTextEditor } from "../../src/RichTextEditor";
+
+function App() {
+  const [content, setContent] = useState("");
+  const readOnly = false;
+
+  return (
+    <div className="main">
+      <h1>RichTextEditor</h1>
+      <br />
+      <RichTextEditor
+        content={content}
+        setContent={setContent}
+        readOnly={readOnly}
+      />
+      <br />
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default App;
