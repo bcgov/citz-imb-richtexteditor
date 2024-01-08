747b714c0cb18bfb98710b55180aa21d4c5058a9
BradyMitch <braden.jr.mitch@gmail.com>
2024-01-08 15:17:48 -0800

    New build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,26 @@
-import dts from "rollup-plugin-dts";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import svg from "rollup-plugin-svg-import";
 
-export default {
-  input: "build/bundle.d.ts",
-  output: {
-    file: "build/index.d.ts",
-    format: "es",
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        dir: "build",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: ["react", "react-dom"],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({ extensions: [".css"] }),
+      svg(),
+      typescript({ tsconfig: "./tsconfig.json", outputToFilesystem: true }),
+    ],
   },
-  plugins: [dts()],
-};
+];
